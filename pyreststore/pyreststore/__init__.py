20c78cc7c4e0658be01df0ba92838b605bcafdc4
Peter Dahl Vestergaard <peter.dahl.vestergaard@gmail.com>
2015-08-05 10:54:35 +0200

    Bump version to development version

--- pyreststore/pyreststore/__init__.py
+++ pyreststore/pyreststore/__init__.py
@@ -5,7 +5,7 @@ __title__ = 'Python implementation of a REST based storage'
 
 # Please use PEP 0440 version strings.
 # https://www.python.org/dev/peps/pep-0440/
-__version__ = '0.9.0'
+__version__ = '0.9.1.dev1'
 __author__ = 'Peter Dahl Vestergaard'
 __license__ = 'BSD 3-Clause'
 __copyright__ = 'Copyright 2015 Peter Dahl Vestergaard'
